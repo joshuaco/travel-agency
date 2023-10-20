6ab7a231fe70894b0dcfad34ae3a3ed59b4444b4
joshua <joxuecont@gmail.com>
2023-10-20 19:23:05 -0400

    Add public assets, styles and layout

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use('/', router);
 // Enable PUG
 app.set('view engine', 'pug');
 
+// Define public folder
+app.use(express.static('public'));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
