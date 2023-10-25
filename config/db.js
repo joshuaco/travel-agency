3a3604d1e0f9e4dd5dee2a6ea4016bdffda698d5
joshua <joxuecont@gmail.com>
2023-10-25 18:30:51 -0400

    Create env file and add to db config

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv/config';
+
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 // Configure database connection
-const db = new Sequelize('agencia_viajes', 'root', 'root', {
-  host: 'localhost',
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
   port: 33061,
   dialect: 'mysql',
   define: {
