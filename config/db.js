fdf5ecf648649d83a20eede0bd5ab79851ebab16
joshua <joxuecont@gmail.com>
2023-10-25 20:15:59 -0400

    Add DB_PORT to db config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv/config';
 
-const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 // Configure database connection
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
-  port: 33061,
+  port: DB_PORT,
   dialect: 'mysql',
   define: {
     timestamps: false,
